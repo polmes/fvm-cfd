e6dd4e6ae1f0831af5895b502a104dbbe2d8120a
Qjammer <baladoramiro+github@gmail.com>
2018-12-10 14:01:03 +0100

    further convective progress

--- VerticalStaggeredVolumes.m
+++ VerticalStaggeredVolumes.m
@@ -14,15 +14,30 @@ classdef VerticalStaggeredVolumes < StaggeredVolumes
 	end
 
 	methods
+		function this = VerticalStaggeredVolumes(MSH)
+			for el=1:size(MSH.REL,2)
+				this.uidx(:,el)=MSH.IDX(1,[MSH.REL(1,el),%1
+										   el,%2
+										   MSH.REL(4,el),%3
+										   MSH.REL(1,MSH.REL(4,el))]);%4
+				this.vidx(:,el)=MSH.IDX(2,[el,
+										   MSH.REL(1,el),
+										   MSH.REL(2,el),
+										   MSH.REL(3,el),
+										   MSH.REL(4,el)]);
+			end
+this.uidx
+		end
+		
 		function Kf = calcKf(this,v,MSH)
-			Kf=sparse(size(this.nodes,2),size(this.nodes,2))
-			for el=1:size(this.nodes,2)
-				n=MSH.COOR(:,this.nodes(:,el))
+			Kf=sparse(size(MSH.REL,2)*3,size(MSH.REL,2)*3)
+			for el=1:size(MSH.REL,2)
+				
 				nv=this.vidx(:,el)
 				nu=this.uidx(:,el)
-				dx=[MSH.dx(el),MSH.dx(MSH.REL(1,el))] %2*NV
-				dy=[MSH.dy(MSH.REL(1,el)),MSH.dy(el),...
-					MSH.dy(MSH.REL(4,el)),MSH.dy(MSH.REL(4,MSH.REL(1,el)))] %4*NV
+				dx=MSH.dx([el,MSH.REL(1,el)]) %2*NV
+				dy=MSH.dy([MSH.REL(1,el),el,...
+				   MSH.REL(4,el),MSH.REL(4,MSH.REL(1,el))]) %4*NV
 
 				Fn=(v(nv(2))+v(nv(1)))*dx(1)/2
 				Fs=(v(nv(4))+v(nv(1)))*dx(2)/2
