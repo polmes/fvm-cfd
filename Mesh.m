8bb17481a3a2eb269f851ffe776b38ae667dd40c
Qjammer <baladoramiro+github@gmail.com>
2018-12-10 14:01:49 +0100

    added IDX to mesh object

--- Mesh.m
+++ Mesh.m
@@ -6,19 +6,21 @@ classdef Mesh
         CN;
         COOR;
         REL;
+		IDX;
 		dx;
 		dy;
     end
     
     methods
-        function obj = Mesh(CN,COOR,REL)
+        function this = Mesh(CN,COOR,REL,IDX)
             %UNTITLED Construct an instance of this class
             %   Detailed explanation goes here
-            obj.CN = CN;
-            obj.COOR = COOR;
-            obj.REL = REL;
-			obj.dx=sqrt(sum((obj.COOR(:,obj.CN(3,:))-obj.COOR(:,obj.CN(4,:))).^2));
-			obj.dy=sqrt(sum((obj.COOR(:,obj.CN(3,:))-obj.COOR(:,obj.CN(2,:))).^2));
+            this.CN = CN;
+            this.COOR = COOR;
+            this.REL = REL;
+			this.IDX = IDX;
+			this.dx=sqrt(sum((this.COOR(:,this.CN(3,:))-this.COOR(:,this.CN(4,:))).^2));
+			this.dy=sqrt(sum((this.COOR(:,this.CN(3,:))-this.COOR(:,this.CN(2,:))).^2));
 		end
     end
 end
