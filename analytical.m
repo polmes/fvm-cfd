02ccd1a0e0a698448735d4f253fff52f2888a437
polmes <pol.developer1@gmail.com>
2018-12-23 20:30:10 +0100

    Code cleanup

--- analytical.m
+++ analytical.m
@@ -1,45 +1,46 @@
-% Manufactured solution setup
+%% PRE
+
+% Manufactured solution
 t = 100; % time [s]
 rho = 1.225; % density [kg/m^3]
 nu = 1.48e-5; % viscosity [m^2/s]
 F = 1; % exp(-8 * pi^2 * nu * t);
 syms x y;
-uv = F * [ cos(2 * pi * x) * sin(2 * pi * y)  ; 
+uvf = F * [ cos(2 * pi * x) * sin(2 * pi * y)  ; 
           -cos(2 * pi * y) * sin(2 * pi * x) ];
-p = -F^2 * rho * (cos(4 * pi * x) + cos(4 * pi * y)) / 4;
+pf = -F^2 * rho * (cos(4 * pi * x) + cos(4 * pi * y)) / 4;
 
-% Analytic convective term
-conv1 = divergence(uv(1) * uv, [x y]);
-conv2 = divergence(uv(2) * uv, [x y]);
+% Analytical convective term
+conv1 = divergence(uvf(1) * uvf, [x y]);
+conv2 = divergence(uvf(2) * uvf, [x y]);
 
-% Analytic diffusive term
-diff1= laplacian(uv(1),[x y]);
-diff2= laplacian(uv(2),[x y]);
+% Analytical diffusive term
+diff1 = laplacian(uvf(1),[x y]);
+diff2 = laplacian(uvf(2),[x y]);
 
-% Init numeric terms
-
+% Numerical parameters
 L = 1;
-X = [0 L];
-Y = [0 L];
+XY = [0 L];
+
+% Loop parameters
 NN = round(logspace(log10(3), 2, 10));
 
+%% LOOP
+
+% Preallocate
 errc = zeros(1, length(NN));
 errd = zeros(1, length(NN));
 
-progress = waitbar(0, '...');
+progress = waitbar(0, 'Increasing Entropy...');
 for k = 1:length(NN)
-	Nx = NN(k);
-	Ny = NN(k);
-	
-	mesh = StructuredMesh(X, Y, Nx, Ny);
+	% Init
+	mesh = SquareMesh(XY, NN(k));
 
-	% Coordinates
-
-	% Horizontal Staggered
+	% Horizontal staggered
 	xh = mean([mesh.coor(1, mesh.cn(1, :)); mesh.coor(1, mesh.cn(2, :))]);
 	yh = mean([mesh.coor(2, mesh.cn(1, :)); mesh.coor(2, mesh.cn(2, :))]);
 
-	% Vertical Staggered
+	% Vertical staggered
 	xv = mean([mesh.coor(1, mesh.cn(1, :)); mesh.coor(1, mesh.cn(4, :))]);
 	yv = mean([mesh.coor(2, mesh.cn(1, :)); mesh.coor(2, mesh.cn(4, :))]);
 
@@ -50,57 +51,61 @@ for k = 1:length(NN)
 	% Analytical convective term
 	Ch = double(subs(conv1, {x, y}, {xh, yh}));
 	Cv = double(subs(conv2, {x, y}, {xv, yv}));
-	Ca = zeros(2, Nx * Ny);
-	Ca(1,:) = Ch;
-	Ca(2,:) = Cv;
+	Ca = [Ch; Cv];
 
 	% Analytical diffusive term
 	Dh = double(subs(diff1, {x, y}, {xh, yh}));
 	Dv = double(subs(diff2, {x, y}, {xv, yv}));
+	Da = [Dh; Dv];
 
-	Da = zeros(2,Nx * Ny);
-	Da(1,:) = Dh;
-	Da(2,:) = Dv;
-
-	% Analytic velocity and pressure fields
-	uvh = double(subs(uv, {x, y}, {xh, yh})); uh = uvh(1, :); % u
-	uvv = double(subs(uv, {x, y}, {xv, yv})); vv = uvv(2, :); % v
-	pp = double(subs(p, {x, y}, {xp, yp}));
-	v = zeros(2,Nx * Ny);
-	v(1,:) = uh;
-	v(2,:) = vv;
-
-	vol = reshape(repelem(mesh.dx .* mesh.dy, 2).',[2 Nx*Ny]);
-
-	% Numerical Convective Term
-
-	Cn = mesh.convective(v) ./ vol;
+	% Velocity field
+	uvh = double(subs(uvf, {x, y}, {xh, yh})); uh = uvh(1, :); % u
+	uvv = double(subs(uvf, {x, y}, {xv, yv})); vv = uvv(2, :); % v
+	uv = [uh; vv];
 	
-	% Numerical Diffusive Term
+	% Pressure field
+	p = double(subs(pf, {x, y}, {xp, yp}));	
 
-	Dn = mesh.diffusive(v) ./ vol;
+	% Numerical convective term
+	Cn = mesh.convective(uv) ./ mesh.vol;
+	
+	% Numerical diffusive term
+% 	Dn = mesh.diffusive(uv) ./ mesh.vol;
 
 	% Errors
-	errc(k) = sqrt(sum(sum((Cn - Ca).^2.*vol)));
-	errd(k) = sqrt(sum(sum((Dn - Da).^2.*vol)));
+	errc(k) = sqrt(sum(sum((Cn - Ca).^2 .* mesh.vol)));
+% 	errd(k) = sqrt(sum(sum((Dn - Da).^2 .* mesh.vol)));
 
-	disp(['ERROR_CONV: ' num2str(errc(k))]);
-	disp(['ERROR_DIFF: ' num2str(errd(k))]);
+	% Print
+	disp(['Iteration #' num2str(k)]);
+	disp(['Error Convective: ' num2str(errc(k))]);
+% 	disp(['Error Diffusive: ' num2str(errd(k))]);
+	disp(' ');
 
 	waitbar(sum(NN(1:k).^2) / sum(NN.^2));
 end
 close(progress);
 
-%% Output data
-figure;
-set(gca, 'XScale', 'log');
-set(gca, 'YScale', 'log');
-hold('on');
-loglog(L./NN, errc);
-loglog(L./NN, errd);
+%% POST
 
-ind = find(errc ~= 0);
-pc = polyfit(log10(L./NN(ind)), log10(errc(ind)), 1);
-pd = polyfit(log10(L./NN(ind)), log10(errd(ind)), 1);
+% Plot
+figure;
+hold('on');
+loglog(L ./ NN, errc);
+% loglog(L ./ NN, errd);
+set(gca, 'XScale', 'log', 'YScale', 'log');
+grid('on');
+xlabel('Grid Size', 'Interpreter', 'latex', 'FontSize', 15);
+ylabel('Error', 'Interpreter', 'latex', 'FontSize', 15);
+% legend('Convetive', 'Diffusive');
+set(findall(gca, 'Type', 'Line'), 'LineWidth', 1);
+set(gca, 'FontSize', 12);
+set(gcf, 'Units', 'centimeters', 'Position', [0 0 21 14]);
+
+% Slopes
+indc = find(errc ~= 0);
+% indd = find(errd ~= 0);
+pc = polyfit(log10(L ./ NN(indc)), log10(errc(indc)), 1);
+% pd = polyfit(log10(L ./ NN(indd)), log10(errd(indd)), 1);
 disp(['Slope Convective: ' num2str(pc(1))]);
-disp(['Slope Diffusive:  ' num2str(pd(1))]);
+% disp(['Slope Diffusive:  ' num2str(pd(1))]);
