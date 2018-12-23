b443216dd4e1564019c6a1539cdfc197b3466342
polmes <pol.developer1@gmail.com>
2018-12-23 14:14:33 +0100

    Overhaul of Mesh structure

--- Mesh.m
+++ Mesh.m
@@ -1,27 +1,8 @@
-classdef Mesh
-    %UNTITLED Summary of this class goes here
-    %   Detailed explanation goes here
-    
+classdef Mesh < handle    
     properties
-        CN;
-        COOR;
-        REL;
-		IDX;
-		dx;
-		dy;
-    end
-    
-    methods
-        function this = Mesh(CN,COOR,REL,IDX)
-            %UNTITLED Construct an instance of this class
-            %   Detailed explanation goes here
-            this.CN = CN;
-            this.COOR = COOR;
-            this.REL = REL;
-			this.IDX = IDX;
-			this.dx=sqrt(sum((this.COOR(:,this.CN(3,:))-this.COOR(:,this.CN(4,:))).^2));
-			this.dy=sqrt(sum((this.COOR(:,this.CN(3,:))-this.COOR(:,this.CN(2,:))).^2));
-		end
-    end
+        coor;
+		cn;
+        rel;		
+		NV;
+	end
 end
-
