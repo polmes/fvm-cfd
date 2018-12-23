a16f772a8dd7937e10801143d065590254c8f5de
polmes <pol.developer1@gmail.com>
2018-12-23 15:26:12 +0100

    Clean up main test script

--- main.m
+++ main.m
@@ -1,30 +1,12 @@
 L = 1;
-X = [0 L];
-Y = [0 L];
+XY = [0 L];
+N = 3;
 
-Nx = 3;
-Ny = 3;
-NV = Nx * Ny;
+% Init
+mesh = SquareMesh(X, N);
 
-mesh = StructuredMesh(X, Y, Nx, Ny);
+% Variables
+uv = ones(2, mesh.NV);
 
-% mesh = StructuredMeshGenerator(X, Y, Nx + 1, Ny + 1).genMesh();
-vsv = VerticalStaggeredVolumes(mesh);
-hsv = HorizontalStaggeredVolumes(mesh);
-% 
-% % Old
-uvp = rand(3 * NV, 1); % ones(3 * Nx * Ny, 1);
-% % u
-% % v
-% % p
-% vol = repelem(mesh.dx .* mesh.dy, 3).';
-% Kch = hsv.calcKc(uvp, mesh);
-% Kcv = vsv.calcKc(uvp, mesh);
-% Cn = (Kch + Kcv) * uvp; %  ./ vol;
-
-% % New
-u = uvp(1:3:end).'; % zeros(1, NV);
-v = uvp(2:3:end).'; % zeros(1, NV);
-p = uvp(3:3:end).'; % zeros(1, NV);
-uv = [u; v];
-c = hsv.convective(mesh, uv);
+% Test
+C = mesh.convective(uv);
