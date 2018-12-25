4e8177179a810f128223eb132dcd1c0a307c91bf
polmes <pol.developer1@gmail.com>
2018-12-25 02:14:33 +0100

    Changed hsv/vsv index order to unify code
    getFlows function now computes convective term flows for both hsv and vsv. The change is also useful for the implementation of the diffusive term

--- StaggeredVolumes.m
+++ StaggeredVolumes.m
@@ -4,22 +4,26 @@ classdef StaggeredVolumes < handle % < Volumes
 	end
 	
 	methods
-		function idx = getNeighbors(~, mesh)
-			idx = [ 1:mesh.NV ;
-					mesh.rel  ];
-		end
-		
 		function U = getVelocities(~, v, idx)
 % 			uu = reshape(v(idx(2:5, :)), 4, []);
 % 			ux = ones(4, 1) * v(1, idx(1, :));
 % 			u = 1/2 * (uu + ux);
 
-			un = v(idx(1, :)) + v(idx(2, :));
-			ue = v(idx(1, :)) + v(idx(3, :));
-			us = v(idx(1, :)) + v(idx(4, :));
-			uw = v(idx(1, :)) + v(idx(5, :));
+			u1 = v(idx(1, :)) + v(idx(2, :));
+			u2 = v(idx(1, :)) + v(idx(3, :));
+			u3 = v(idx(1, :)) + v(idx(4, :));
+			u4 = v(idx(1, :)) + v(idx(5, :));
 			
-			U = 1/2 * [un; ue; us; uw];
+			U = 1/2 * [u1; u2; u3; u4];
+		end
+		
+		function F = getFlows(~, u, v, uidx, vidx, dx, dy)			
+			f1 = (u(uidx(1, :)) + u(uidx(2, :))) .* dy(uidx(1, :));
+			f2 = (u(uidx(1, :)) + u(uidx(3, :))) .* dy(uidx(1, :));
+			f3 = v(vidx(1, :)) .* dx(vidx(1, :)) + v(vidx(2, :)) .* dx(vidx(2, :));
+			f4 = v(vidx(3, :)) .* dx(vidx(3, :)) + v(vidx(4, :)) .* dx(vidx(4, :));
+			
+			F = 1/2 * [f1; -f2; f3; -f4];
 		end
 	end
 end
--- VerticalStaggeredVolumes.m
+++ VerticalStaggeredVolumes.m
@@ -1,36 +1,35 @@
 classdef VerticalStaggeredVolumes < StaggeredVolumes
 	properties
 		uidx; % 4xNV
-		% 4 1
-		% 3 2
+		% 3 1
+		% 4 2
 		vidx; % 5xNV
 		%   2
-		% 5 1 3
-		%   4
+		% 5 1 4
+		%   3
 	end
 
 	methods
 		function this = VerticalStaggeredVolumes(mesh)
-			this.vidx = this.getNeighbors(mesh);
+			this.vidx = [ 1:mesh.NV      ;
+						  mesh.rel(1, :) ;
+						  mesh.rel(3, :) ;
+						  mesh.rel(2, :) ;
+						  mesh.rel(4, :) ];
 			
 			this.uidx = [ mesh.rel(1, :)               ;
 			              1:mesh.NV                    ;
-			              mesh.rel(4, :)               ;
-			              mesh.rel(1, mesh.rel(4, :)) ];
+			              mesh.rel(1, mesh.rel(4, :))  ;
+						  mesh.rel(4, :)               ];
 		end
 
 		function c = convective(this, mesh, uv)
+			% [n; s; e; w]
 			V = this.getVelocities(uv(2, :), this.vidx);
+			F = this.getFlows(uv(2, :), uv(1, :), this.vidx, this.uidx, mesh.dy, mesh.dx);
 			
-			Fn = (uv(2, this.vidx(1, :)) + uv(2, this.vidx(2, :))) .* (mesh.dx(this.vidx(1, :)) + mesh.dx(this.vidx(3, :))) / 2;
-			Fs = (uv(2, this.vidx(1, :)) + uv(2, this.vidx(4, :))) .* (mesh.dx(this.vidx(1, :)) + mesh.dx(this.vidx(4, :))) / 2;
-			Fe = uv(1, this.uidx(1, :)) .* mesh.dx(this.uidx(1, :)) + uv(1, this.uidx(2, :)) .* mesh.dx(this.uidx(2, :));
-			Fw = uv(1, this.uidx(3, :)) .* mesh.dx(this.uidx(3, :)) + uv(1, this.uidx(4, :)) .* mesh.dx(this.uidx(4, :));
-			F = 1/2 * [Fn; Fe; -Fs; -Fw];
-			
-% 			c = 1/4 * (ue .* Fe - uw .* Fw + un .* Fn - us .* Fs);
+			% c = 1/4 * (ue .* Fe - uw .* Fw + un .* Fn - us .* Fs);
 			c = sum(V .* F, 1);
-
 		end
 	end
 end
