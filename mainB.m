81773b25b27ad95101918be8e8fd3e4396bcf199
polmes <pol.developer1@gmail.com>
2018-12-31 03:25:58 +0100

    Fixed velocity correction with new function and minor changes
    Now finds middle points for initial conditions too

--- mainB.m
+++ mainB.m
@@ -1,17 +1,33 @@
+%% PRE
+
 % Init
 L = 1;
 XY = [0 L];
 N = 10;
 mesh = msh.SquareMesh(XY, N);
 
+% Find middle points
+[~, indmc] = min( sum( abs(mesh.coor - L/2), 1 ) );
+indmv = find(mesh.cn(4, :) == indmc);
+if mod(mesh.Nx, 2) == 0
+	% is even
+	indm = [0 1] + indmv;
+else
+	% is odd
+	indm = [0 2] + indmv;
+end
+
+%% TEST
+
 % Arbitrary velocity field that fulfills global mass conservation
 uv = zeros(2, mesh.NV);
-uv(2, round(0.5 * (mesh.NV - N) + 1)) = +1;
-uv(2, round(0.5 * (mesh.NV - N) - 1)) = -1;
+uv(2, indm) = [-1 +1];
 
 % Corrected velocity field
-uvcorr = uv - mesh.correction(uv);
+uvcorr = mesh.correction(uv);
 
-% Post
-plotQuiver(mesh, uv);
-plotQuiver(mesh, uvcorr);
+%% POST
+
+% Plot velocity field: before & after
+util.quiver(mesh, uv);
+util.quiver(mesh, uvcorr);
