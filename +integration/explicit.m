7d147150c65a194245845f6a7445817b3010cad9
polmes <pol.developer1@gmail.com>
2018-12-31 03:22:55 +0100

    Now computes pressure field and checks CFL condition
    Switched NT/it to dt/dti and changed velocity correction

--- +integration/explicit.m
+++ +integration/explicit.m
@@ -1,37 +1,83 @@
-function [uvt,tr]= explicit(mesh,uvi,mu,T,tn,itsave)
-
-% Fluid Properties
-
-t=linspace(T(1),T(2),tn+1);
-dt=t(2:end)-t(1:end-1);
-
-uvt=zeros(floor(tn/itsave)+1,size(uvi,1),size(uvi,2));
-
-uv=uvi-mesh.correction(uvi);
-C=mesh.convective(uv);
-D=mesh.diffusive(uv);
-Rn=(-C+mu*D)./mesh.vol;
-Ro=Rn;
-uvt(1,:,:)=uv;
-for k=1:size(dt,2)
-	
-	C=mesh.convective(uv);
-	D=mesh.diffusive(uv);
-	Rn=(-C+mu*D)./mesh.vol;
-	uvnew=uv+dt(k)*(1.5*Rn-0.5*Ro);
-	uvcorr=uvnew-mesh.correction(uvnew);
-	
-	if(mod(k,itsave)==0)
-		display(['Iteration ',num2str(k)]);
-		uvt(floor(1+k/itsave),:,:)=uvcorr;
+function [ts, uvt, pt] = explicit(mesh, uv0, rho, nu, T, dt, dti)
+	% Check input
+	if mod(dti, dt) ~= 0
+		dti = dt * floor(dti / dt);
+		warning(['dti is not divisible by dt. Switching to dti = ' num2str(dti) ' seconds.']);
 	end
-	if(~isempty(find(isnan(uvcorr))))
-		display(['NaN exception at k=',num2str(k),', t=',num2str(t(k))])
-		k
-		return
+	
+	% Save every # of iterations
+	it = dti / dt; % == NT / NS
+	
+	% Init time vectors
+	t = unique([T(1):dt:T(2) T(2)]);
+	NT = length(t);
+	ts = unique([T(1):dti:T(2) T(2)]);
+	NS = length(ts);
+	
+	% Init solution vectors
+	uvt = zeros(2, mesh.NV, NS);
+	pt  = zeros(1, mesh.NV, NS);
+	
+	% Print start
+	disp('Starting simulation');
+	disp(['t = ' num2str(t(1)) ' seconds']);
+	disp(' ');
+	
+	% n = 1
+	[uvn, pp] = mesh.correction(uv0);
+	uvt(:, :, 1) = uvn;
+	pt(:, :, 1) = rho / dt * pp;
+	
+	% do-for
+	C = mesh.convective(uvn);
+	D = mesh.diffusive(uvn);
+	Rprev = (nu * D - C) ./ mesh.vol;
+	
+	% n > 1
+	for k = 2:NT
+		% Convective and Diffusive terms
+		C = mesh.convective(uvn);
+		D = mesh.diffusive(uvn);
+		Rn = (nu * D - C) ./ mesh.vol;
+		
+		% Adams-Bashforth
+		uvpred = uvn + dt * (3/2 * Rn - 1/2 * Rprev);
+		[uvnext, pp] = mesh.correction(uvpred);
+		
+		% CFL condition
+		CFL = max( dt * (uvnext(1, :) ./ mesh.dx + uvnext(2, :) ./ mesh.dy) );
+		
+		if any(isnan(uvnext))
+			error(['NaN exception at k = ' num2str(k)]);
+		end
+		
+		if mod(t(k), dti) == 0
+			% Save values
+			uvt(:, :, 1 + (k - 1) / it) = uvnext;
+			pt(:, :, 1 + (k - 1) / it) = rho / dt * pp;
+			
+			% Print
+			disp(['Iteration #' num2str(k)]);
+			disp(['t = ' num2str(t(k)) ' seconds']);
+			disp(['Max CFL = ' num2str(CFL)]);
+			disp(' ');
+		end
+		
+		if CFL > 1
+			warning(['CFL exceeded 1 at k = ' num2str(k)]);
+		end
+		
+		Rprev = Rn;
+		uvn = uvnext;
 	end
-	uv=uvcorr;
-	Ro=Rn;
+	
+	% Save last value if necessary
+	if mod(t(NT), dti) ~= 0
+		uvt(:, :, NS) = uvnext;
+		pt(:, :, NS) = rho / dt * pp;
+	end
+	
+	% Print end
+	disp('Simulation ended');
+	disp(['t = ' num2str(t(NT)) ' seconds']);
 end
-
-tr=t([1,find(mod(1:k,itsave)==0)]);
