288e41d6fe75cc9e893058c6c03360b9d050dc64
polmes <pol.developer1@gmail.com>
2019-01-05 03:37:11 +0100

    Add convergence study: uv(t) and mesh
    Computes the max error in the velocity with different grid sizes and plots the convergence of uv as a f(t) for the max number of elements

--- mainC.m
+++ mainC.m
@@ -2,46 +2,120 @@
 
 % Fluid properties
 rho = 1.225; % density [kg/m^3]
-nu = 0.1; % 1.48e-5; % kinematic viscosity [m^2/s]
+nu = 0.1; % kinematic viscosity [m^2/s]
 
 % Mesh setup
-L = 1; % [m]
-N = 10;
+L = 1; % mesh size [m]
+XY = [0 L];
 
 % Time setup
-tf = 0.05; % 0.52; % 0.05; % [s]
-dt = 1e-5; % 1e-2; % 1e-3; % time-step [s]
-dti = 2e-3; % 1e-2; % save every # of iterations
+tf = 0.5; % simulation time [s]
+dt = 1e-3; % time-step [s]
+dti = 2e-2; % save every # of iterations [s]
+T = [0 tf]; 
 
-% Initial conditions
-v0 = 1; % [m/s]
+% Manufactured solution
+uvf = util.analytical(rho, nu);
+
+% Loop parameters
+NN = [5 10 20]; % # of elements
 
 %% LOOP
 
-% Init mesh
-XY = [0 L];
-mesh = msh.SquareMesh(XY, N);
+% Preallocate
+err = zeros(1, length(NN));
 
-% Find middle points
-[~, indmc] = min( sum( abs(mesh.coor - L/2), 1 ) );
-indmv = find(mesh.cn(4, :) == indmc);
-if mod(mesh.Nx, 2) == 0
-	% is even
-	indm = (0:1) + indmv;
-else
-	% is odd
-	indm = (0:2) + indmv;
+progress = waitbar(0, 'Increasing Entropy...');
+for k = 1:length(NN)
+	%% START
+	
+	% Init
+	disp(['Starting convergence iteration #' num2str(k)]);
+	disp(' ');
+	mesh = msh.SquareMesh(XY, NN(k));
+
+	% Horizontal staggered
+	xh = mean([mesh.coor(1, mesh.cn(2, :)); mesh.coor(1, mesh.cn(4, :))]);
+	yh = mean([mesh.coor(2, mesh.cn(2, :)); mesh.coor(2, mesh.cn(4, :))]);
+
+	% Vertical staggered
+	xv = mean([mesh.coor(1, mesh.cn(3, :)); mesh.coor(1, mesh.cn(4, :))]);
+	yv = mean([mesh.coor(2, mesh.cn(3, :)); mesh.coor(2, mesh.cn(4, :))]);
+	
+	% Initial conditions
+	uvh0 = uvf(0, xh, yh); uh0 = uvh0(1, :); % u
+	uvv0 = uvf(0, xv, yv); vv0 = uvv0(2, :); % v
+	uv0 = [uh0; vv0];
+	
+	%% NUMERICAL
+	
+	% Time integration
+	[t, uvt, pt] = integration.explicit(mesh, uv0, rho, nu, T, dt, dti);
+	
+	%% ANALYTICAL
+
+	% Repeat arrays
+	NT = length(t);
+	tt = repmat(reshape(t, [1 1 NT]), [1 mesh.NV]);
+	xxh = repmat(xh, [1 1 NT]);
+	yyh = repmat(yh, [1 1 NT]);
+	xxv = repmat(xv, [1 1 NT]);
+	yyv = repmat(yv, [1 1 NT]);
+
+	% Velocity field
+	uvh = uvf(tt, xxh, yyh); uh = uvh(1, :, :); % u(t)
+	uvv = uvf(tt, xxv, yyv); vv = uvv(2, :, :); % v(t)
+	uva = [uh; vv]; % uv(t)
+	
+	%% END
+	
+	% Error
+	err(k) = max(max(max(uvt - uva)));
+
+	% Print
+	disp(' ');
+	disp(['Convergence iteration #' num2str(k) ' ended']);
+	disp(['Error: ' num2str(err(k))]);
+	disp(' ');
+
+	waitbar(sum(NN(1:k).^2) / sum(NN.^2));
 end
-
-% Init velocity
-uv0 = zeros(2, mesh.NV);
-uv0(2, indm) = v0;
-
-% Time integration
-T = [0 tf];
-[t, uvt, pt] = integration.explicit(mesh, uv0, rho, nu, T, dt, dti);
+close(progress);
 
 %% POST
 
+% Animation
 util.render(mesh, t, pt);
 util.render(mesh, t, uvt);
+
+% Convergence of u(t), v(t) in element #1
+el = 1;
+figure;
+hold('on');
+plot(t, reshape(uvt(1, el, :), 1, []), '-x');
+plot(t, reshape(uva(1, el, :), 1, []), '-+');
+plot(t, reshape(uvt(2, el, :), 1, []), '-x');
+plot(t, reshape(uva(2, el, :), 1, []), '-+');
+grid('on');
+xlabel('Time [s]', 'Interpreter', 'latex', 'FontSize', 15);
+ylabel('Velocity [m/s]', 'Interpreter', 'latex', 'FontSize', 15);
+legend({'$u_\mathrm{numerical}$', '$u_\mathrm{analytical}$', '$v_\mathrm{numerical}$', '$v_\mathrm{analytical}$'}, ...
+	'Interpreter', 'latex', 'FontSize', 15, 'Location', 'northeast');
+set(findall(gca, 'Type', 'Line'), 'LineWidth', 1);
+set(gca, 'FontSize', 12);
+set(gcf, 'Units', 'centimeters', 'Position', [0 0 21 14]);
+
+% Plot grid convergence
+h = L ./ NN; % grid size
+figure;
+hold('on');
+loglog(h, err, 'd-', 'MarkerFaceColor', 'auto');
+loglog(h, h.^2);
+set(gca, 'XScale', 'log', 'YScale', 'log');
+grid('on');
+xlabel('Grid Size', 'Interpreter', 'latex', 'FontSize', 15);
+ylabel('Error', 'Interpreter', 'latex', 'FontSize', 15);
+legend({'Velocity', 'h^2'}, 'Location', 'northwest');
+set(findall(gca, 'Type', 'Line'), 'LineWidth', 1);
+set(gca, 'FontSize', 12);
+set(gcf, 'Units', 'centimeters', 'Position', [0 0 21 14]);
